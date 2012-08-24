232d27656f120cc51763d75c4d0f9e4174e6b000
jmiller <jmiller@jmilbox42.mm-corp.net>
2012-08-23 21:44:21 -0400

    initial load, project in progress

---
+++ pysdeecurl.py
@@ -0,0 +1,33 @@
+import sys
+import pycurl
+import base64
+from pysdee import idsmxml
+from time import sleep
+
+ip = "the.ip.add.ress"
+sdeeUrl = "https://$ip/cgi-bin/sdee-server"
+sdeeUser = "username"
+sdeePassword = "password"
+
+
+def getSubscription(url, user, password):
+    "Establishes the re-usable session to the IPS module"
+#	assemble the URL
+#	connect and get a result
+#	store the subId
+
+
+def parseEvents():
+    "Examines and parses out the returned XML into event fields"
+
+
+def getEvents():
+    "Gets events using an established subscriptionId"
+#	assemble the URL
+#	consider how many to get
+#	if $subId exists get next events in subscription
+#		connect and get a result
+#		getSubscription()
+#		connect and get first results in subscription
+#	endif
+#	parseEvents()
